0bf366827aabe2a56e183b84c093f0a77fd9ad98
hiigami <jpkarty@gmail.com>
2021-09-28 18:19:25 -0500

    add example test for unknown schema and for not_defined return value

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,40 @@
-jest.unmock("../src/random");
+import Processor from "../src/processor";
+const _processor = Processor.getInstance();
+type ProcessorType = typeof _processor;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as rnd from "../src/random";
+jest.mock("../src/processor");
 
 import { AnySchema } from "yup";
 
 import yh from "../src/main";
+import { NOT_DEFINED } from "../src/strategies/constant";
+
+const getInstance = Processor.getInstance as jest.MockedFunction<
+  typeof Processor.getInstance
+>;
+
+function getMockProcessor(returnValue: unknown) {
+  return { run: jest.fn((_schema) => returnValue) } as unknown as ProcessorType;
+}
+
+const dummySchema = { type: "1234" } as AnySchema;
+
+beforeAll(() => {
+  getInstance.mockReset();
+});
 
 test("should return undefined for unhandled schemas", () => {
-  const val = yh.example({ type: "1234" } as AnySchema);
+  const mockProcessor = getMockProcessor(undefined);
+  getInstance.mockReturnValue(mockProcessor);
+  const val = yh.example(dummySchema);
+  expect(val).toEqual(undefined);
+});
+
+test("should return undefined when NOT_DEFINED symbol is drawn", () => {
+  const mockProcessor = getMockProcessor({
+    draw: jest.fn((_options) => NOT_DEFINED),
+  });
+  getInstance.mockReturnValue(mockProcessor);
+  const val = yh.example(dummySchema);
   expect(val).toEqual(undefined);
 });
