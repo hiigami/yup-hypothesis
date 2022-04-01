eb59d56a72fe68c88b015c87fb62fb72bb57202d
hiigami <jpkarty@gmail.com>
2022-03-31 19:22:30 -0600

    add test for mixed.withMutation (#18)

--- __tests__/mixed.test.ts
+++ __tests__/mixed.test.ts
@@ -1,10 +1,10 @@
-jest.unmock("../src/random");
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as rnd from "../src/random";
-
 import * as yup from "yup";
+
+import * as rnd from "../src/random";
 import { testXTimes } from "./utils";
+
+jest.unmock("../src/random");
 
 test("should render mixed", async () => {
   const TestSchema = yup.object({
